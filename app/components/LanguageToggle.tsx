b7b0588afbf48920fc0f52148de6b14940aa4a83
JonasThowsen <jonas.aa.thowsen@gmail.com>
2024-09-16 23:28:13 +0200

    endret language slider

--- app/components/LanguageToggle.tsx
+++ app/components/LanguageToggle.tsx
@@ -1,17 +1,29 @@
 'use client';
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLanguage } from '../context/LanguageContext';
 
 const LanguageToggle: React.FC = () => {
   const { language, toggleLanguage } = useLanguage();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const handleToggle = () => {
+    toggleLanguage();
+  };
+
+  if (!isClient) {
+    return null; // or a loading placeholder
+  }
 
   return (
     <div className="flex items-center">
       <span className={`mr-2 ${language === 'en' ? 'font-bold' : ''}`}>EN</span>
       <div
         className="w-14 h-7 flex items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out cursor-pointer"
-        onClick={toggleLanguage}
+        onClick={handleToggle}
       >
         <div
           className={`bg-white w-5 h-5 rounded-full shadow-md transform duration-300 ease-in-out ${

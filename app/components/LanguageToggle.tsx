6c15098ec22b58300e0c28711b9c8c9e6e443cb0
JonasThowsen <jonas.aa.thowsen@gmail.com>
2024-09-17 00:10:48 +0200

    prøver ny versjon av language slider

--- app/components/LanguageToggle.tsx
+++ app/components/LanguageToggle.tsx
@@ -14,16 +14,11 @@ const LanguageToggle = () => {
     <div className="flex items-center">
       <span className={`mr-2 ${language === 'en' ? 'font-bold' : ''}`}>EN</span>
       <button
-        className="w-14 h-7 flex items-center bg-gray-300 rounded-full p-1 forst"
+        className={`w-14 h-7 flex items-center rounded-full toggle-wrapper`}
         onClick={toggleLanguage}
         aria-label={`Switch to ${language === 'en' ? 'Norwegian' : 'English'}`}
-      >
-        <div
-          className={`bg-white w-5 h-5 rounded-full shadow-md transform sist ${
-            language === 'no' ? 'translate-x-7' : 'translate-x-0'
-          }`}
-        />
-      </button>
+        data-language={language}
+      />
       <span className={`ml-2 ${language === 'no' ? 'font-bold' : ''}`}>NO</span>
     </div>
   );

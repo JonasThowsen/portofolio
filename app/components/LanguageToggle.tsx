5fe7a957a5fce72555375f70b22933150c5a6550
JonasThowsen <jonas.aa.thowsen@gmail.com>
2024-09-17 09:46:59 +0200

    endret til svart skrift

--- app/components/LanguageToggle.tsx
+++ app/components/LanguageToggle.tsx
@@ -3,20 +3,21 @@ import { useLanguage } from '../context/LanguageContext';
 
 const LanguageToggle = () => {
   const { language, setLanguage } = useLanguage();
+
   const toggleLanguage = () => {
     setLanguage(language === 'en' ? 'no' : 'en');
   };
 
   return (
     <div className="language-toggle-container">
-      <span className={`mr-2 ${language === 'en' ? 'font-bold' : ''}`}>EN</span>
+      <span className={`mr-2 text-black ${language === 'en' ? 'font-bold' : ''}`}>EN</span>
       <button
         className={`w-14 h-7 flex items-center rounded-full toggle-wrapper`}
         onClick={toggleLanguage}
         aria-label={`Switch to ${language === 'en' ? 'Norwegian' : 'English'}`}
         data-language={language}
       />
-      <span className={`ml-2 ${language === 'no' ? 'font-bold' : ''}`}>NO</span>
+      <span className={`ml-2 text-black ${language === 'no' ? 'font-bold' : ''}`}>NO</span>
     </div>
   );
 };
